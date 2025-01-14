0d5e6852b8192b16478473dc41b835b50e71e33f
daman599 <168351935+daman599@users.noreply.github.com>
2025-01-14 13:01:34 +0000

    var keyword

--- tott.js
+++ tott.js
@@ -1,10 +1,15 @@
-console.log("hey there");
+//console.log("hey there");
 
 //variables
-{
+
 const id = 3456;
 let email = "saj@gmail.com";
 var name = "daman";
-}
+contact = 9890245;
+
 // console.log(id);
-console.log(name);
+//console.log(name);
+//console.log(contact)
+
+id=30000;
+console.table([id,name,email,contact]);
