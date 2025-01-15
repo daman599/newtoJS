af791e0d47ce356a7dcec67bd4535bf9766d0c15
daman599 <168351935+daman599@users.noreply.github.com>
2025-01-15 14:30:01 +0000

    add datatypes

--- tott.js
+++ tott.js
@@ -11,5 +11,15 @@ contact = 9890245;
 //console.log(name);
 //console.log(contact)
 
-id=30000;
-console.table([id,name,email,contact]);
+//id=30000;
+//console.table([id,name,email,contact]);
+
+//datatype 
+let age;
+let logged=true;
+let add = null;
+//console.log(age);
+let n = 785399210294958755720570575n
+console.log(typeof age)
+
+
