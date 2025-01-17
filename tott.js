c6dea9da04153d2b13309ecce5e389e0def7c5c5
daman599 <168351935+daman599@users.noreply.github.com>
2025-01-17 16:09:00 +0000

    learnings

--- tott.js
+++ tott.js
@@ -20,6 +20,26 @@ let logged=true;
 let add = null;
 //console.log(age);
 let n = 785399210294958755720570575n
-console.log(typeof age)
+//console.log(typeof age)
+
+//memmory
+//stack memory
+let num = 67;
+console.log(num);
+/*
+let newNumber=num;
+newNumber=78
+console.log(newNumber);
+console.log(num);
+*/
+
+//heap memory
+let myname=["a","m","a","n"];
+//console.log(myname);
+
+let dog=myname;
+
+//console.log(dog)
+//learnt about arrays ,objects,number ,math ,library
 
 
